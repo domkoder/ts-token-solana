2d730e7f23d114569af3f5b75ea22e26a390fd96
Nandom Alfred <nandommamdam@gmail.com>
2024-02-05 04:04:05 +0100

    Using the createV1 method to create metadata account

---
+++ src/main.ts
@@ -0,0 +1,39 @@
+import {
+	TokenStandard,
+	createV1,
+	mintV1,
+	mplTokenMetadata,
+} from '@metaplex-foundation/mpl-token-metadata'
+import {
+	generateSigner,
+	keypairIdentity,
+	percentAmount,
+} from '@metaplex-foundation/umi'
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults'
+import { userKeypair } from './helpers'
+
+const umi = createUmi('https://api.devnet.solana.com')
+
+const keypair = umi.eddsa.createKeypairFromSecretKey(userKeypair.secretKey)
+umi.use(keypairIdentity(keypair)).use(mplTokenMetadata())
+
+const metadata = {
+	name: 'Sol Way',
+	symbol: 'SOLW',
+	uri: 'https://raw.githubusercontent.com/solana-developers/program-examples/new-examples/tokens/tokens/.assets/spl-token.json',
+}
+
+const mint = generateSigner(umi)
+
+async function createMetadataDetails() {
+	await createV1(umi, {
+		mint,
+		authority: umi.identity,
+		name: metadata.name,
+		symbol: metadata.symbol,
+		uri: metadata.uri,
+		sellerFeeBasisPoints: percentAmount(0),
+		decimals: 9,
+		tokenStandard: TokenStandard.Fungible,
+	}).sendAndConfirm(umi)
+}
